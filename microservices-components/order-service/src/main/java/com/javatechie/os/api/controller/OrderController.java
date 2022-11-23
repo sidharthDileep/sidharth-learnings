491d448fa312c5fddc1940cb9f000d56f2753ad3
Sidharth PP <sidharthdileep96gmail.com>
2022-11-23 12:39:40 +0530

    added new microservices project

--- microservices-components/order-service/src/main/java/com/javatechie/os/api/controller/OrderController.java
+++ microservices-components/order-service/src/main/java/com/javatechie/os/api/controller/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
 @RequestMapping("/order")
@@ -19,7 +20,7 @@ public class OrderController {
     private OrderService service;
     
     @PostMapping("/bookOrder")
-    public TransactionResponse bookOrder(@RequestBody TransactionRequest request) {
+    public TransactionResponse bookOrder(@RequestBody TransactionRequest request) throws JsonProcessingException{
         return service.saveOrder(request);
     }
 }
